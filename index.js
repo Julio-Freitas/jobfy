ac58b75688aece379dc725f4de0353328db6375f
Julio Freitas <julio.clf31@gmail.com>
2019-08-16 13:19:01 -0300

    fixed views path

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000 ;
-const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise });
 
+const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise });
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
